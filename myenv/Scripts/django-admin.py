1a646de5b1186317cf9f5493cd5ae9ae4744ec65
Eduardo Luis Ferreira Reis <eduardoluisreis@gmail.com>
2019-01-03 13:10:36 -0200

    My ISO41001 app, fisrt commit

---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\as\desktop\eduardo reis\webiso\iso41001\myenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
